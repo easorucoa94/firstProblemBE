b992981f1e54abb843681503dcfb68b7b6f8da8c
Andres Soruco <easorucoa94@gmail.com>
2020-02-07 18:09:11 -0400

    Initial Commit. (#1)

---
+++ firstProblem/src/main/java/com/truextend/firstProblem/FirstProblemApplication.java
@@ -0,0 +1,13 @@
+package com.truextend.firstProblem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstProblemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstProblemApplication.class, args);
+	}
+
+}
