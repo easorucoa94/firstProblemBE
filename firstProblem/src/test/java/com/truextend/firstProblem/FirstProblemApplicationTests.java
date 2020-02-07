f8e8e4859c23144800d156a9713e2be5c35cc5c5
root <root@easorucoa94-System-Product-Name>
2020-02-07 18:08:11 -0400

    Initial Commit.

---
+++ firstProblem/src/test/java/com/truextend/firstProblem/FirstProblemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.truextend.firstProblem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstProblemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
