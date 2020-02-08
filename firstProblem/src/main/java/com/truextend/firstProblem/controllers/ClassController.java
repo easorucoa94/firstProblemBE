2e021f841bcde41e7bec07a3e42aa01bbb494567
root <root@easorucoa94-System-Product-Name>
2020-02-07 22:13:37 -0400

    - Add Class delete method

--- firstProblem/src/main/java/com/truextend/firstProblem/controllers/ClassController.java
+++ firstProblem/src/main/java/com/truextend/firstProblem/controllers/ClassController.java
@@ -6,7 +6,9 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,5 +33,11 @@ public class ClassController {
 	public ResponseEntity<ClassEntity> save(@Valid @RequestBody ClassEntity classEntity) {
 		return ResponseEntity.ok(classRepository.save(classEntity));
 	}
+	
+	@DeleteMapping("/delete/{sClass_id}")
+    public void delete(@PathVariable String sClass_id) {
+		Long lClass_id = Long.parseLong(sClass_id);
+        classRepository.deleteById(lClass_id);
+    }
 	
 }
