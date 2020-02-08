618de43fed38fd2abfc7066514d04ac87a6309ca
root <root@easorucoa94-System-Product-Name>
2020-02-07 20:27:39 -0400

    - Add class ID to entity - Add creation methods to controller

--- firstProblem/src/main/java/com/truextend/firstProblem/entities/ClassEntity.java
+++ firstProblem/src/main/java/com/truextend/firstProblem/entities/ClassEntity.java
@@ -7,16 +7,27 @@ import javax.persistence.*;
 public class ClassEntity {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long lClass_id;
 	private String sClass_code;
 	private String sClass_title;
 	private String sClass_description;
 	
 	protected ClassEntity() {}
 	
-	public ClassEntity(String sClass_code, String sClass_title, String sClass_description) {
+	public ClassEntity(Long lClass_id, String sClass_code, String sClass_title, String sClass_description) {
+		this.lClass_id = lClass_id;
 		this.sClass_code = sClass_code;
 		this.sClass_title = sClass_title;
 		this.sClass_description = sClass_description;
+	}
+	
+	public Long getLClass_id() {
+		return this.lClass_id;
+	}
+	
+	public void setLClass_id(Long lClass_id) {
+		this.lClass_id = lClass_id;
 	}
 	
 	public String getSClass_code() {
