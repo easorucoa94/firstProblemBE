51593914e5ae206caa5724bab46336e1e115c370
Andres Soruco <easorucoa94@gmail.com>
2020-02-07 20:28:32 -0400

    Add Class creation methods (#6)
    * - Make it to not take underscores at naming table columns
    
    * - Add class ID to entity
    - Add creation methods to controller

--- firstProblem/src/main/java/com/truextend/firstProblem/controllers/ClassController.java
+++ firstProblem/src/main/java/com/truextend/firstProblem/controllers/ClassController.java
@@ -2,8 +2,13 @@ package com.truextend.firstProblem.controllers;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +25,11 @@ public class ClassController {
 	@GetMapping
 	public List<ClassEntity> findAll() {
 		return classRepository.findAll();
+	}
+	
+	@PostMapping(consumes = "application/json")
+	public ResponseEntity<ClassEntity> save(@Valid @RequestBody ClassEntity classEntity) {
+		return ResponseEntity.ok(classRepository.save(classEntity));
 	}
 	
 }
